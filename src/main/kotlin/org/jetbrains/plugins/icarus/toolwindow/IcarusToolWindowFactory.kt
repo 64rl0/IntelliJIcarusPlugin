9abc4970e7a7587c5978c4f9155d13bceb4a11a9
carlogtt <carlogtt@amazon.com>
2026-02-28 19:10:45 +0000

    carlogtt updates

--- src/main/kotlin/org/jetbrains/plugins/icarus/toolwindow/IcarusToolWindowFactory.kt
+++ src/main/kotlin/org/jetbrains/plugins/icarus/toolwindow/IcarusToolWindowFactory.kt
@@ -1,11 +1,12 @@
 package org.jetbrains.plugins.icarus.toolwindow
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 
-class IcarusToolWindowFactory : ToolWindowFactory {
+class IcarusToolWindowFactory : ToolWindowFactory, DumbAware {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         project.service<IcarusOutputService>().initialize(toolWindow)
